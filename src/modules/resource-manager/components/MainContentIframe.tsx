faa3d54cf9bc39e7f2ffd93a3bead27f81abbce1
ShinMini <gusals121234@gmail.com>
2023-10-14 21:55:26 +0900

    feat(screen): main iframe section

--- src/modules/resource-manager/components/MainContentIframe.tsx
+++ src/modules/resource-manager/components/MainContentIframe.tsx
@@ -1,21 +1,34 @@
-import { useAppSelector } from '../hooks/useRedux';
+import styled from '@emotion/styled';
+import { useAppDispatch, useAppSelector } from '../hooks/useRedux';
+import CloseIcon from './../assets/icons/close.svg';
+
+const Title = styled.h1`
+	color: #000;
+	font-family: Roboto;
+	font-size: 14px;
+	font-style: normal;
+	font-weight: 400;
+	line-height: normal;
+`;
 
 const MainContentIframe = () => {
-	// const [iframe, setIframe] = React.useState<HTMLIFrameElement | null>(null);
-	const {resource } = useAppSelector(state => state);
+	const currentUrl  = useAppSelector(state => state.resource.currentUrl);
+	const dispatch = useAppDispatch();
+
+	const onClickCloseIcon = () => {
+		dispatch({ type: 'resource/SET_CURRENT_URL', payload: ''});
+	};
 
 	return (
-		<div>{resource.resourceList.map((value, index) => {
-
-			return (
-				<div key={index}>
-					<p className="text-lg">
-						{value.resource.src}
-					</p>
-				</div>
-			);
-		}
-		)}</div>
+		<div className='flex flex-col w-full h-screen'>
+			<header className='flex w-full justify-between items-center box-border px-3 py-2 bg-white shadow h-12'>
+				<Title>{currentUrl}</Title>
+				<img src={CloseIcon} alt='close-icon' className='cursor-pointer' onClick={onClickCloseIcon} />
+			</header>
+			<section className="w-full h-full">
+				<iframe className='w-full h-full' src={currentUrl} referrerPolicy='same-origin'/>
+			</section>
+		</div>
 	);
 };
 
