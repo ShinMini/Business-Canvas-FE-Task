5da49392f4f4807c5be5b53310e5e9a293ac88af
ShinMini <gusals121234@gmail.com>
2023-10-14 17:03:04 +0900

    feat(config): ts, lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ViteSvg from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [ViteSvg(), react()],
 	server: {
 		port: 3000,
 		open: true,
